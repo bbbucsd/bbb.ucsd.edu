f4a0aaa1528acf60de0ec21c60d1d6eba0ff2fc5
Eric Raio <ericraio@gmail.com>
2018-07-19 15:37:24 -0700

    removing everything

--- wordpress/wp-content/themes/postlight-headless-wp/inc/shortcodes/keytakeaways.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function ayb_keytakeaways( $options, $content ) {
-  return "<div class='keytakeaways'><h3>Key Takeaways</h3>" . $content . "</div>";
-}
-
-add_shortcode('keytakeaways', 'ayb_keytakeaways');
