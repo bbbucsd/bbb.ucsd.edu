f9407ac72a1d4d8a20b56269b3301bbed992baad
Eric Raio <ericraio@gmail.com>
2018-07-29 16:59:47 -0700

    comment out for now

--- src/components/Meta/FrontMatter/fragment.js
+++ src/components/Meta/FrontMatter/fragment.js
@@ -1,8 +1,8 @@
-export const OpenGraph = graphql`
-  fragment MetaFrontMatterFieldsFields on data_3 {
-    site_title
-    meta_description
-    meta_robots_advanced
-    display_in_search_results
-  }
-`
+//export const OpenGraph = graphql`
+  //fragment MetaFrontMatterFieldsFields on data_3 {
+    //site_title
+    //meta_description
+    //meta_robots_advanced
+    //display_in_search_results
+  //}
+//`
