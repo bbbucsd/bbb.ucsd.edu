07d1ab31c06ca6d5a72ae3b11379be13f6bb719b
Eric Raio <ericraio@gmail.com>
2018-07-29 14:54:32 -0700

    removing keywords

--- src/components/Meta/FrontMatter/fragment.js
+++ src/components/Meta/FrontMatter/fragment.js
@@ -1,7 +1,6 @@
 export const OpenGraph = graphql`
   fragment MetaFrontMatterFieldsFields on data_3 {
     site_title
-    meta_keywords
     meta_description
     meta_robots_advanced
     display_in_search_results
--- src/components/Meta/Helper.js
+++ src/components/Meta/Helper.js
@@ -4,7 +4,6 @@ class MetaHelper {
   static FrontMatterFields(data) {
     return {
       site_title: data.site_title,
-      meta_keywords: data.meta_keywords,
       meta_description: data.meta_description,
       meta_robots_advanced: data.meta_robots_advanced,
       display_in_search_results: data.display_in_search_results
