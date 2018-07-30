304807ad083abf0ea2dc54c1446c062073a228c8
Eric Raio <ericraio@gmail.com>
2018-07-29 17:31:32 -0700

    adding guard if direction is not passed

--- src/components/Elements/Block/BlockContainer/index.js
+++ src/components/Elements/Block/BlockContainer/index.js
@@ -4,20 +4,20 @@ import React, { Component } from 'react';
 import List from '@material-ui/core/List';
 
 // Style
-import style from './style.module.scss'
+import style from './style.module.scss';
 
 class BlockContainer extends Component {
   constructor(props) {
-    super(props)
-    this.blocks = React.Children.toArray(props.children)
+    super(props);
+    this.blocks = React.Children.toArray(props.children);
 
-    if (!!props.direction.match(/left/i)) {
-      this.blocks.reverse()
+    if (props.direction && !!props.direction.match(/left/i)) {
+      this.blocks.reverse();
     }
   }
 
   render() {
-    const { children } = this.props
+    const { children } = this.props;
 
     return (
       <div className={ style.root }>
