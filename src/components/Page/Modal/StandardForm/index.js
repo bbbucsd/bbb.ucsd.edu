44d6d737a5dbb9f1be8dab4a036b8d753f94d314
Eric Raio <ericraio@gmail.com>
2018-08-01 10:37:44 -0700

    adding a progress bar for a simple sense of urgency

--- src/components/Page/Modal/StandardForm/index.js
+++ src/components/Page/Modal/StandardForm/index.js
@@ -6,8 +6,47 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import LinearProgress from '@material-ui/core/LinearProgress';
+import style from './style.module.scss'
+import classNames from 'classnames/bind';
+let cx = classNames.bind(style);
 
 class StandardForm extends Component {
+
+  timer = null;
+
+  state = {
+    completed: 0,
+    buffer: 10,
+  };
+
+  componentDidMount() {
+    this.timer = setInterval(this.progress, 500);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  progress = () => {
+    const { completed } = this.state;
+    if (completed < 50 ) {
+      const diff = Math.random() * 10;
+      const diff2 = Math.random() * 10;
+      this.setState({ completed: completed + diff, buffer: completed + diff + diff2 });
+    }
+  };
+
+  renderProgressBar() {
+    const { completed, buffer } = this.state;
+
+    return (
+      <div className={style.progressBarRoot}>
+        <LinearProgress variant="buffer" value={completed} valueBuffer={buffer} />
+        <br />
+      </div>
+    );
+  }
 
   render() {
     return (
@@ -18,6 +57,7 @@ class StandardForm extends Component {
       >
         <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
         <DialogContent>
+          {this.renderProgressBar()}
           <DialogContentText>
             To subscribe to this website, please enter your email address here. We will send
             updates occasionally.
@@ -52,6 +92,7 @@ export const query = graphql`
   fragment StandardForm on PrismicModalBodyStandardForm {
     slice_type
     primary {
+      progress_bar_enabled
       headline
     }
   }
