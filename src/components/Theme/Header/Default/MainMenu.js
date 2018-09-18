42f2158a0736dbce3060c7834f3100c0c41426ce
Eric Raio <ericraio@gmail.com>
2018-09-18 04:20:30 -0700

    test again

--- src/components/Theme/Header/Default/MainMenu.js
+++ src/components/Theme/Header/Default/MainMenu.js
@@ -1,9 +1,6 @@
 import React, { Fragment, Component } from 'react';
 import StartHere from './MainMenu/StartHere';
 import About from './MainMenu/About';
-import Learn from './MainMenu/Learn';
-//import Coaching from './MainMenu/Coaching';
-import Resources from './MainMenu/Resources';
 import Contact from './MainMenu/Contact';
 
 
@@ -15,11 +12,6 @@ class MainMenu extends Component {
       <Fragment>
         <StartHere floating={floating} color={color} />
         <About floating={floating} color={color} />
-        <Learn floating={floating} color={color} />
-        <Resources floating={floating} color={color} />
-        {/*
-        <Coaching floating={floating} color={color} />
-        */}
         <Contact floating={floating} color={color} />
       </Fragment>
     );
