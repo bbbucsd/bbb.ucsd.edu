c4c6ed16da26eaf6fd9d72d8e1238bf1b5362b91
Eric Raio <ericraio@gmail.com>
2018-10-14 15:21:46 -0700

    adding missing file

---
+++ src/components/Theme/Header/Default/MainMenu/Books.js
@@ -0,0 +1,32 @@
+
+import React, { Component } from 'react'
+import Link from 'components/Theme/Link'
+import { styled } from 'components/Theme/Styles';
+
+const Wrapper = styled.div`
+  position: relative;
+  padding: 20px;
+  white-space: nowrap;
+`;
+
+const MainLink = styled(Link)`
+  font-weight: 400;
+  color:${props => props.floating ? props.theme.white : (props.color || props.theme.white)};
+  &:visited {
+    color:${props => props.floating ? props.theme.white : (props.color || props.theme.white)};
+  }
+`;
+
+export default class extends Component {
+
+  render() {
+    const { floating, color } = this.props;
+
+    return (
+      <Wrapper>
+        <MainLink color={color} floating={floating} to="/books">Books</MainLink>
+      </Wrapper>
+    );
+  }
+
+}
